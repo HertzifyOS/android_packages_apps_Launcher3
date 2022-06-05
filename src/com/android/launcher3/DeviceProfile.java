a1f2e5134a9dee9fd19eeff8bc9b93e59ea64b13
Pranav Vashi <neobuddy89@gmail.com>
2022-06-05 13:14:15 +0530

    Launcher3: Make drawer cells square when labels are hidden
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -121,6 +121,7 @@ public class DeviceProfile {
     private FolderProfile mFolderProfile;
     private AllAppsProfile mAllAppsProfile;
     private final OverviewProfile overviewProfile;
+    private final boolean mAllAppsIconText;
     private final float mAllAppsCellHeightMultiplier;
 
     // Hotseat
@@ -195,6 +196,7 @@ public class DeviceProfile {
         mAllAppsProfile = new AllAppsProfile(new Point(0, 0), 0, 0, 0f, 0, 0, 0, 0, 0, 0,
                 new Rect(), 0, 0);
         mAllAppsCellHeightMultiplier = 1f;
+        mAllAppsIconText = true;
         mSysuiProfile = new SysuiProfile(0, 0, false);
     }
 
@@ -238,6 +240,7 @@ public class DeviceProfile {
         final Resources res = context.getResources();
 
         mAllAppsCellHeightMultiplier = LauncherPrefs.ROW_HEIGHT.get(context) / 100f;
+        mAllAppsIconText = LauncherPrefs.SHOW_DRAWER_LABELS.get(context);
 
         overviewProfile = OverviewProfile.Factory.createOverviewProfile(res);
 
@@ -422,7 +425,21 @@ public class DeviceProfile {
             hideWorkspaceLabelsIfNotEnoughSpace();
         }
 
+        if (!mAllAppsIconText) {
+            int leftRightPadding =
+                    getWorkspaceProfile().getDesiredWorkspaceHorizontalMarginPx();
+            int drawerWidth =
+                    mDeviceProperties.getAvailableWidthPx() - leftRightPadding * 2;
+            int cellWidth =
+                    drawerWidth / mAllAppsProfile.getNumShownAllAppsColumns();
+            int cellHeight =
+                    (int) (cellWidth * mAllAppsCellHeightMultiplier);
+
+            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
+        }
+
         if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
+                && mAllAppsIconText
                 && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
             // Add extra textHeight to the existing allAppsCellHeight.
             mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(
