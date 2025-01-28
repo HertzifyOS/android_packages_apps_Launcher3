8d0d07f2f24e2a8df5b14be77cea361cc46ff609
Frederico Silva <fredericojssilva@gmail.com>
2025-01-28 23:43:17 +0000

    Launcher3: Implement auto open keyboard in drawer setting
    Add setting to always show keyboard when swiping up on your home screen
    
    Change-Id: Ib2cdbcf23d5b46dc44a1641215f20c1ae20c4731
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -18,6 +18,7 @@ package com.android.launcher3;
 
 import static com.android.launcher3.LauncherPrefs.ALLAPPS_THEMED_ICONS;
 import static com.android.launcher3.LauncherPrefs.DB_FILE;
+import static com.android.launcher3.LauncherPrefs.DRAWER_OPEN_KEYBOARD;
 import static com.android.launcher3.LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE;
 import static com.android.launcher3.LauncherPrefs.FIXED_LANDSCAPE_MODE;
 import static com.android.launcher3.LauncherPrefs.GRID_NAME;
@@ -304,6 +305,7 @@ public class InvariantDeviceProfile {
                     && com.android.systemui.shared.Flags.workspaceItemsLabelHidden()) {
                 onConfigChanged();
             } else if (ALLAPPS_THEMED_ICONS.getSharedPrefKey().equals(key) ||
+                    DRAWER_OPEN_KEYBOARD.getSharedPrefKey().equals(key) ||
                     SHOW_DESKTOP_LABELS.getSharedPrefKey().equals(key) ||
                     SHOW_DRAWER_LABELS.getSharedPrefKey().equals(key)) {
                 onConfigChanged();
