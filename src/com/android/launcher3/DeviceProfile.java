62879f2e2fe0b13810828eac4dda69e9d34a29d3
shutter-cat <bankersenator@gmail.com>
2026-03-07 18:09:40 +0200

    Launcher3: Introduce appdrawer styles
    normal, fullscreen, paged, list, homescreen only
    
    @neobuddy89:
    * Register shared preference observer onCreate instead onResume
    * Add strings for translations
    * Rewrite few strings
    * Removed ios style.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -61,6 +61,7 @@ import com.android.launcher3.deviceprofile.HotseatProfile;
 import com.android.launcher3.deviceprofile.OverviewProfile;
 import com.android.launcher3.deviceprofile.TaskbarProfile;
 import com.android.launcher3.deviceprofile.WorkspaceProfile;
+import com.android.launcher3.allapps.AppDrawerStyle;
 import com.android.launcher3.icons.DotRenderer;
 import com.android.launcher3.model.data.ItemInfo;
 import com.android.launcher3.responsive.CalculatedCellSpec;
@@ -543,7 +544,7 @@ public class DeviceProfile {
                 mInsets,
                 res,
                 mWorkspaceProfile.getEdgeMarginPx(),
-                shouldShowAllAppsOnSheet(),
+                shouldShowAllAppsOnSheet(context),
                 mWorkspaceProfile
         );
 
@@ -557,8 +558,7 @@ public class DeviceProfile {
             hotseatBorderSpace = getWorkspaceIconProfile().getCellLayoutBorderSpacePx().y;
         }
 
-
-        if (shouldShowAllAppsOnSheet()) {
+        if (shouldShowAllAppsOnSheet(context)) {
             allAppsPadding.top = allAppsTopPadding;
             allAppsShiftRange = mDeviceProperties.getHeightPx() - allAppsTopPadding + mInsets.top;
         } else {
@@ -983,7 +983,13 @@ public class DeviceProfile {
     }
 
     /** Whether All Apps should be presented on a bottom sheet. */
-    public boolean shouldShowAllAppsOnSheet() {
+    public boolean shouldShowAllAppsOnSheet(Context context) {
+        if (context != null) {
+            String style = AppDrawerStyle.get(context);
+            if (AppDrawerStyle.isVerticalPaged(style) || AppDrawerStyle.isFullscreen(style)) {
+                return false;
+            }
+        }
         return mDeviceProperties.isTablet() || Flags.allAppsSheetForHandheld();
     }
 
