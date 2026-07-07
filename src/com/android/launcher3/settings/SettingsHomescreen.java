Lunaris-AOSP/packages_apps_Launcher3

Launcher3: Add optional toggle for app launch blur animation
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -142,7 +142,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER_CITY.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER_PROVIDER.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER_TEXT.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.AUTO_HIDE_DOTS.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.AUTO_HIDE_DOTS.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.APP_LAUNCH_BLUR_ENABLED.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }

HertzifyOS/android_packages_apps_Launcher3

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: HDzungx <hdzungx@gmail.com>

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -137,7 +137,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER_CITY.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_QUICKSPACE_WEATHER_PROVIDER.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.SHOW_QUICKSPACE_WEATHER_TEXT.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.SHOW_QUICKSPACE_WEATHER_TEXT.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.APP_LAUNCH_BLUR_ENABLED.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }